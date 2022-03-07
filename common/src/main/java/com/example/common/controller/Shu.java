64955d48bbe8b8cf2bd72f57a2a6e4f2958d3803
yangxin96 <15071714190@163.com>
2022-03-07 19:29:25 +0800

    重构项目

---
+++ common/src/main/java/com/example/common/controller/Shu.java
@@ -0,0 +1,21 @@
+package com.example.common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * yang
+ * 2022/3/7
+ * 4:45 PM
+ **/
+
+@RequestMapping("/hai/")
+@RestController
+public class Shu {
+
+    @GetMapping("he")
+    public String he(){
+        return "简单的测试";
+    }
+}
